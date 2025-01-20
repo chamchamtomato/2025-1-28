1ae16bce898e6b133186a5c9b43155d86e53e12a
chamcham0707 <cheamin6306@naver.com>
2025-01-20 11:52:24 +0900

    git init

---
+++ src/test/java/org/chaego/baek/BaekApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chaego.baek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
